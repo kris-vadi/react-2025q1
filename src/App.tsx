aa97356ad29462091d50010a046165ce094fe129
Kristina Morozova <christina.vadi@gmail.com>
2025-01-31 09:52:18 +0500

    fix: error boundary button functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
+import { Component, ReactNode } from "react";
 import "./App.css";
 import Button from "./components/Button/Button";
 import Header from "./components/Header/Header";
 
-function App() {
-  return (
-    <>
-      <Header />
-      <main className="main">
-        <Button>Throw Error</Button>
-      </main>
-    </>
-  );
+class App extends Component {
+  state = {
+    hasError: false,
+  };
+
+  throwErrorBoundary = () => {
+    this.setState({ hasError: true });
+  };
+
+  render(): ReactNode {
+    if (this.state.hasError) {
+      throw new Error("ErrorBoundary worked!");
+    }
+
+    return (
+      <>
+        <Header />
+        <main className="main">
+          <Button onClick={this.throwErrorBoundary}>Throw Error</Button>
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;
