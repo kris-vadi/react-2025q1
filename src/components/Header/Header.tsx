69d7ad6b9705000fa89d12c03f6a4a9c8630f7ad
Kristina Morozova <christina.vadi@gmail.com>
2025-01-31 09:31:06 +0500

    feat: add Button component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,13 @@
 import { Component, ReactNode } from "react";
 import styles from "./Header.module.css";
+import Button from "../Button/Button";
 
 class Header extends Component {
   render(): ReactNode {
     return (
       <header className={styles.header}>
         <div className={styles.logo}></div>
+        <Button>send</Button>
       </header>
     );
   }
