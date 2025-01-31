ac57cd5fddd74616271ff2d52ba37a2df6e1119f
Kristina Morozova <christina.vadi@gmail.com>
2025-01-31 09:41:23 +0500

    feat: add error boundary button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Button from "./components/Button/Button";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <main className="main">
+        <Button>Throw Error</Button>
+      </main>
     </>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,11 @@
 import { Component, ReactNode } from "react";
 import styles from "./Header.module.css";
-import Button from "../Button/Button";
 
 class Header extends Component {
   render(): ReactNode {
     return (
       <header className={styles.header}>
         <div className={styles.logo}></div>
-        <Button>send</Button>
       </header>
     );
   }
